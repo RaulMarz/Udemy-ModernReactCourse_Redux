168e1c528b5c4e3f4625903f7b09e65878510bad
RaulMarz <raul.martinez@publicisone.com>
2018-08-23 17:11:35 -0500

    Section:4-Video:50

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+    return [
+        {title:'Book 1', pages: 101},
+        {title:'Book 2', pages: 102},
+        {title:'Book 3', pages: 103},
+        {title:'Book 4', pages: 104}
+    ]
+};
